a3f17fbaa3485c751cb56a6967240461c92a770f
Anton Beloglazov <anton.beloglazov@gmail.com>
2014-04-07 09:03:38 +1000

    Switch root to use fish shell as well

--- 02-fish.sh
+++ 02-fish.sh
@@ -4,3 +4,4 @@ brew install fish
 grep -q '^/usr/local/bin/fish$' /etc/shells; or echo '/usr/local/bin/fish' | \
     sudo tee -a /etc/shells
 chsh -s /usr/local/bin/fish
+sudo chsh -s /usr/local/bin/fish
