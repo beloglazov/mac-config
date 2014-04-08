80595d4f06338c2dee1cb312cebd1a72bc44f074
Anton Beloglazov <anton.beloglazov@gmail.com>
2014-04-08 14:45:51 +1000

    Added cabal and ghc-mod

--- install.sh
+++ install.sh
@@ -8,7 +8,12 @@ brew tap homebrew/dupes
 
 brew install coreutils findutils gnu-tar gnu-sed gawk gnutls gnu-indent \
     gnu-getopt grep fish htop colorsvn lftp maven tree ranger colordiff ncdu \
-    fasd the_silver_searcher dtrx wget ctags vim
+    fasd the_silver_searcher dtrx wget ctags vim cabal-install
+
+cabal update
+cabal install cabal-install
+cabal update
+cabal install ghc-mod
 
 ranger --copy-config=scope
 
