d23d00321ddf037bb48438d6e588e43780902cf7
Anton Beloglazov <anton.beloglazov@gmail.com>
2014-06-16 16:30:11 +1000

    Added rbenv configuration

--- home/.config/fish/path.fish
+++ home/.config/fish/path.fish
@@ -1,3 +1,6 @@
 set PATH /Users/anton/.cabal/bin /usr/local/opt/coreutils/libexec/gnubin /usr/local/bin /usr/local/sbin $PATH
 set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
 set -x JAVA_HOME (/usr/libexec/java_home -v 1.7)
+
+set -gx RBENV_ROOT /usr/local/var/rbenv
+. (rbenv init -|psub)
